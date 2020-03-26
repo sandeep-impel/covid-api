eaca2ef8fa54949547b026ac489e75cc89ede9dd
Sandeep YSV <sandeep@impel.io>
2020-03-26 12:23:53 +0000

    Log Requests to a File

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const log = require('log-to-file');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.post('/',function(req, res, next) {
+	log(JSON.stringify(req.body), 'requests.log');
 	console.log(req.body);
 	res.status(200).json({});
 });
